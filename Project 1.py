8dea2a7eb38209b270f612056c905cc3532268e6
Katherine Pollan <sunshinenorcas@gmail.com>
2023-11-28 17:27:50 -0500

    updating code and cleaning up folder

--- Project 1.py
+++ Project 1.py
@@ -1,34 +1,46 @@
-#program opening
-print("Hello!")
-print("Please create a username and password.")
-print("Username must be between 5 and 13 characters, cannot be taken, and contain only letters.")
-print("Password must be between 8 and 16 characters and contain at least one uppercase letter.")
-
 #list for usernames
-verified_usernames = ["admin","testuser","useruser"]
+verified_usernames = ["admin","admin123","root"]
 #list for passwords
 verified_passwords = ["Adminpass1!,tesTpass3","pasSs123"]
 
+#set for allowed username characters
+allowed_username_characters = set("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_")
+#set to test if password has allowed characters
+special_password_characters = set("!?@#^&*_-")
+
+#program opening
+print("Hello!")
+print("Please create a username and password.")
+print("Username must:")
+print("Start with a lower case letter/n Only contain letters, numbers and underscores")
+print("Not be already in use")
+print("Password must:")
+print("Be at least 8 letters long /n Contains at least 1 uppercase letter/n Contains 1 lowercase letter")
+print("Contains at least one digit/n Contains at least one of these characters:!,?,@,#,^,&,*,_,- ")
+print("Doesn't contain any spaces")
+
 #user input for username
-username_input = input("Please enter a username: ")
+username_input = input("Please enter a username: ") #gathers user's proposed handle
 #sanitize user's input for username
 sani_username_input = username_input.strip()
 
 #start Username verfifaction
-#checking username length
-username_length = len(sani_username_input)
-if sani_username_input == username_length <5 or username_length >13:
-    print("Username must be between 5 and 13 characters.")
+#checking if username starts with lowercase
+username_lowercase = sani_username_input.lower(0)
+if username_lowercase == False:
+    print("Username must start with a lowercase letter.")
     print("Please try again.")
-else:
-   continue
+    
+   else:
+    continue
 
-#checking if username only has letters
-username_alpha = sani_username_input.isalpha()
-if username_alpha == False:
-  print("Username can only contain letters")
+#checking if username has letters, numbers and underscores
+usernameinput_validation = set((sani_username_input))
+if usernameinput_validation.issubset(allowed_username_characters) == False:
+  print("Username can only contain letters, numbers, and underscores")
   print("Please try again.")
-else:
+  
+  else:
    continue
 
 #checking if username is taken
@@ -36,7 +48,7 @@ if sani_username_input in verified_usernames == True:
   print("That username is taken.")
   print("Please try again.")
 else:
-   continue
+   #continue
 
 #gathering user input for password
 userpassword_input = input("Please enter a password:" )
@@ -47,11 +59,11 @@ sani_userpass_input = userpassword_input.strip()
 #start Password verifaction
 #checking password length
 password_length = len(sani_userpass_input)
-if sani_userpass_input == password_length <8 or password_length >16:
-  print("Password must be between 8 and 16 characters.")
+if sani_userpass_input == password_length <8:
+  print("Password must be at least 8 characters.")
   print("Please try again.")
 else:
-   continue
+   #continue
 
 #checking if password has an uppercase letter
 password_uppercase = sani_userpass_input.isupper()
@@ -59,7 +71,7 @@ if password_uppercase == False:
   print("Password must contain at least one uppercase letter")
   print("Please try again")
 else:
-   continue
+   #continue
 
 #checking if password has a lowercase letter
 password_lowercase = sani_userpass_input.islower()
@@ -67,14 +79,29 @@ if password_lowercase == False:
   print("Password must contain at least one lowercase letter")
   print("Please try again")
 else:
-   continue
+   #continue
+
+#checking to see if password has a number
+password_number = sani_userpass_input.isdigit()
+if password_number == False:
+  print("Password must contain at least one number")
+  print("Please try again")
+
+password_specialcharacters = set((sani_userpass_input))
+if usernameinput_validation.issubset(special_password_characters) == False:
+  print ("Password must contain one of these characters: !?@#^&*_-")
+  print("Please try again.")
+else:
+   #continue
+
 #User has completed verifaction
+
 
 #add username and passwords to list
 verified_usernames.append(sani_username_input)
 verified_passwords.append(sani_userpass_input)
 
-print("Thank you! Your user name and password are verified.")
+print("Thank you! Sign up successful.")
 print("Please log in.")
 
 #gathering users login input
@@ -91,3 +118,5 @@ if sani_inputusername in verified_usernames == True and sani_inputpassword in ve
 else:
   print("Incorrect user name or password")
   print("Please try again.")
+
+#create zip+dictonary to verify that username+pass go together
